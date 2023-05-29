df1c7ba8e8fbf9ee60b0d7b209a31b47a80980a2
Kamil Kozioł <koziol.kamil@protonmail.com>
2023-05-29 07:29:38 +0200

    lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<CR>'] = cmp.mapping.confirm({select = true}),
   ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
