add188145bff74c34b013b76031742b068da8548
Kamil Koziol <kkoziol@internships.antmicro.com>
2024-09-23 14:30:36 +0200

    Hybrid oil and nvimtree

--- lua/goat/plugins/oil.lua
+++ lua/goat/plugins/oil.lua
@@ -7,6 +7,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
     require("oil").setup()
-    vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
