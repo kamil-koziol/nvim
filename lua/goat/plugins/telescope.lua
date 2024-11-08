aaa447aa1ea0164b9f3b2895abeed405f0522636
Kamil Koziol <kkoziol@internships.antmicro.com>
2024-11-08 13:55:48 +0100

    Added find git files

--- lua/goat/plugins/telescope.lua
+++ lua/goat/plugins/telescope.lua
@@ -37,8 +37,6 @@ return {
       },
       pickers = {
         find_files = {
-          no_ignore = true,
-          hidden = true,
           file_ignore_patterns = {
             "bin",
             "node_modules",
@@ -59,7 +57,13 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git-files in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fa",
+      "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>",
+      { desc = "Fuzzy find all files in cwd" }
+    )
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
