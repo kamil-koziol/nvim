c833d7cae68639ce925ec655696eec1d1f574a85
Kamil Koziol <kkoziol@internships.antmicro.com>
2025-04-01 10:53:57 +0200

    format colorscheme.lua

--- lua/goat/plugins/colorscheme.lua
+++ lua/goat/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
-return { 
-  'olivercederborg/poimandres.nvim',
+return {
+  "olivercederborg/poimandres.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require('poimandres').setup {}
+    require("poimandres").setup({})
   end,
 
   -- optionally set the colorscheme within lazy config
   init = function()
     vim.cmd("colorscheme poimandres")
     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#222233" })
-    vim.api.nvim_set_hl(0, 'MatchParen', { bg = '#222222', fg = '#ffffff', bold = true })
-  end
+    vim.api.nvim_set_hl(0, "MatchParen", { bg = "#222222", fg = "#ffffff", bold = true })
+  end,
 }
