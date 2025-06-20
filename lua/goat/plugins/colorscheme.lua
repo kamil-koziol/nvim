e8cbbc53e420ec7e25575396d5ba28e0a359dd1c
Kamil Koziol <koziol.kamil@protonmail.com>
2025-06-20 13:42:23 +0200

    colorscheme imperial

--- lua/goat/plugins/colorscheme.lua
+++ lua/goat/plugins/colorscheme.lua
@@ -1,15 +1,9 @@
 return {
-  "olivercederborg/poimandres.nvim",
+  "kamil-koziol/imperial.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("poimandres").setup({})
-  end,
-
-  -- optionally set the colorscheme within lazy config
-  init = function()
-    vim.cmd("colorscheme poimandres")
-    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#222233" })
-    vim.api.nvim_set_hl(0, "MatchParen", { bg = "#222222", fg = "#ffffff", bold = true })
+    vim.cmd.colorscheme('imperial')
+    -- vim.api.nvim_set_hl(0, "MatchParen", { bg = "#222222", fg = "#ffffff", bold = true })
   end,
 }
