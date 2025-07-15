964d5a4e143373c349b4e1dafb72d728fd6ecdc2
Kamil Koziol <koziol.kamil@protonmail.com>
2025-07-15 23:36:19 +0200

    add fzf-lua keybinds

--- lua/goat/plugins/fzf-lua.lua
+++ lua/goat/plugins/fzf-lua.lua
@@ -3,7 +3,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
   config = function()
-    vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find git-files in cwd" })
+    vim.keymap.set("n", "<leader>fa", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find all files in cwd" })
+    vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua git_files<cr>", { desc = "Fuzzy find git-files in cwd" })
     vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep_native<cr>", { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>gh", "<cmd>FzfLua git_bcommits<cr>", { desc = "Git commits history" })
   end,
 }
