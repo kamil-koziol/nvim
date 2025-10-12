ed61c73bf5bf65ac71ffe290a903fa2a8464ff81
Kamil Koziol <koziol.kamil@protonmail.com>
2025-10-12 10:51:38 +0200

    fterm: add lf to search for files

--- lua/goat/plugins/fterm.lua
+++ lua/goat/plugins/fterm.lua
@@ -3,12 +3,12 @@ return {
     lazy = true,
     keys = {
         { "<leader>lg", desc = "Open LazyGit in Floating Terminal" },  -- lazy-load on key
+        { "<leader>lf", desc = "Open LF in Floating Terminal" },
     },
     config = function()
       local FTerm = require("FTerm")
 
       FTerm.setup({
-        border = 'double',
         dimensions = { width = 0.9, height = 0.9 },
       })
 
@@ -16,15 +16,35 @@ return {
       local lazygit = FTerm:new({
         cmd = "lazygit",
         dimensions = { width = 0.9, height = 0.9 },
-        border = "double",
         auto_close = true,
         cwd = vim.fn.getcwd(),
       })
 
-      -- Keybinding to toggle LazyGit
+      local lf = FTerm:new({
+          cmd = "sh -c 'lf -print-selection > /tmp/lf_selection'", -- save selection to temp file
+          dimensions = { width = 0.9, height = 0.9 },
+          auto_close = true,
+          cwd = vim.fn.getcwd(),
+          on_exit = function()
+              local f = io.open("/tmp/lf_selection", "r")
+              if f then
+                  local file = f:read("*l")
+                  f:close()
+                  os.remove("/tmp/lf_selection")
+                  if file and file ~= "" then
+                      vim.cmd("edit " .. file)
+                  end
+              end
+          end,
+      })
+
       vim.keymap.set("n", "<leader>lg", function()
         lazygit:toggle()
       end, { desc = "Open LazyGit in Floating Terminal" })
+
+      vim.keymap.set("n", "<leader>lf", function()
+        lf:toggle()
+      end, { desc = "Open LF in Floating Terminal" })
     end,
 }
 
